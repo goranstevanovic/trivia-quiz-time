6889b1d74bed3ec58ea035e5d51e4cb7036fb114
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-13 12:10:25 +0200

    Extend ESLint with Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
