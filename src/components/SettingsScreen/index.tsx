8d2873f955d7410689ff8560c2b543856f662a67
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-14 16:52:17 +0200

    Add Start Quiz button to SettingsScreen

--- src/components/SettingsScreen/index.tsx
+++ src/components/SettingsScreen/index.tsx
@@ -1,9 +1,11 @@
+import Button from '../Button';
 import SettingsForm from '../SettingsForm';
 
 export default function SettingsScreen() {
   return (
     <div>
       <SettingsForm />
+      <Button>Start Quiz</Button>
     </div>
   );
 }
