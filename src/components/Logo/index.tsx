d4cf8ac6fdc109e994ffde3d8b2a9311810309b5
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-14 15:42:21 +0200

    Style Logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,9 +1,17 @@
+import styles from './Logo.module.css';
+
 export default function Logo() {
   return (
-    <div>
-      <p>It's</p>
-      <h1>Trivia Quiz Time</h1>
-      <p>Test your knowledge</p>
+    <div className={styles.logoContainer}>
+      <p className={styles.supHeading}>It's</p>
+      <h1 className={styles.heading}>
+        Trivia
+        <br />
+        Quiz
+        <br />
+        Time
+      </h1>
+      <p className={styles.subHeading}>Test your knowledge</p>
     </div>
   );
 }
