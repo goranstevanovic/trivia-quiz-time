f480ce45f30191b0d84a12209ed568fce00f5bbc
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-17 16:41:36 +0200

    Add sample content to Question component

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,3 +1,28 @@
+const question = {
+  category: 'General Knowledge',
+  type: 'multiple',
+  difficulty: 'easy',
+  question:
+    'Which of the following card games revolves around numbers and basic math?',
+  correct_answer: 'Uno',
+  incorrect_answers: ['Go Fish', 'Twister', 'Munchkin'],
+};
+
+const answers = [question.correct_answer, ...question.incorrect_answers];
+
 export default function Question() {
-  return <div>Question</div>;
+  return (
+    <div>
+      <header>
+        <p>{question.category}</p>
+        <p>{question.difficulty}</p>
+      </header>
+      <p>{question.question}</p>
+      <ul>
+        {answers.map((answer) => (
+          <li>{answer}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
