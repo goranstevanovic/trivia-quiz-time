8e4f1f5ef1b393a78d03092eca9498a80cea20c2
Goran Stevanovic <110386061+goranstevanovic@users.noreply.github.com>
2023-07-17 17:59:58 +0200

    Create Question component (#14)
    Created a Question component that displays the question, answer options, category, and difficulty.

---
+++ src/components/Question/index.tsx
@@ -0,0 +1,28 @@
+import styles from './Question.module.css';
+
+export default function Question() {
+  return (
+    <div className={styles.questionWrapper}>
+      <header className={styles.questionMeta}>
+        <p>General Knowledge</p>
+        <p>Easy (10 points)</p>
+      </header>
+      <p className={styles.questionText}>
+        Which of the following card games revolves around numbers and basic
+        math?
+      </p>
+      <ul className={styles.answerOptions}>
+        <li className={styles.answerOption}>Uno</li>
+        <li className={`${styles.answerOption} ${styles.answerCorrect}`}>
+          Go Fish
+        </li>
+        <li className={`${styles.answerOption} ${styles.answerSelected}`}>
+          Twister
+        </li>
+        <li className={`${styles.answerOption} ${styles.answerIncorrect}`}>
+          Munchkin
+        </li>
+      </ul>
+    </div>
+  );
+}
