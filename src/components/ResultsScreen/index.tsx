9df058dd808ce4f7151b35098aa3a45131aa4812
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-17 19:06:26 +0200

    Style ResultsScreen component

--- src/components/ResultsScreen/index.tsx
+++ src/components/ResultsScreen/index.tsx
@@ -1,17 +1,18 @@
 import Button from '../Button';
+import styles from './ResultsScreen.module.css';
 
 export default function ResultsScreen() {
   return (
-    <div>
-      <div>
-        <h1>Results</h1>
+    <div className={styles.resultsScreen}>
+      <div className={styles.resultsWrapper}>
+        <h1 className={styles.resultsHeading}>Results</h1>
         <div>
-          <h2>Total correct answers:</h2>
-          <p>40/50 &ndash; 80%</p>
+          <h2 className={styles.resultsTitle}>Total correct answers:</h2>
+          <p className={styles.resultsStats}>40/50 &ndash; 80%</p>
         </div>
         <div>
-          <h2>Total points:</h2>
-          <p>1,000/1,500 &ndash; 67%</p>
+          <h2 className={styles.resultsTitle}>Total points:</h2>
+          <p className={styles.resultsStats}>1,000/1,500 &ndash; 67%</p>
         </div>
       </div>
 
