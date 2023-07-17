444c8e7d053d0d1610a799a0ae489e090532658d
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-17 18:52:04 +0200

    Add sample content

--- src/components/ResultsScreen/index.tsx
+++ src/components/ResultsScreen/index.tsx
@@ -1,3 +1,21 @@
+import Button from '../Button';
+
 export default function ResultsScreen() {
-  return <div>ResultsScreen</div>;
+  return (
+    <div>
+      <div>
+        <h1>Results</h1>
+        <div>
+          <h2>Total correct answers:</h2>
+          <p>40/50 &ndash; 80%</p>
+        </div>
+        <div>
+          <h2>Total points:</h2>
+          <p>1,000/1,500 &ndash; 67%</p>
+        </div>
+      </div>
+
+      <Button>Play Again</Button>
+    </div>
+  );
 }
