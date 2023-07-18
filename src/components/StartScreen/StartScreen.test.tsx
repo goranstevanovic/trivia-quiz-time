2f48b32a02d7b70b7ca6bd692e51aa608002bd76
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-18 12:53:18 +0200

    Wrap StartScreen with QuizProvider

--- src/components/StartScreen/StartScreen.test.tsx
+++ src/components/StartScreen/StartScreen.test.tsx
@@ -2,16 +2,25 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import StartScreen from '.';
+import { QuizProvider } from '../../contexts/QuizContext';
 
 describe('StartScreen', () => {
   it('should render Logo component', () => {
-    render(<StartScreen />);
+    render(
+      <QuizProvider>
+        <StartScreen />
+      </QuizProvider>,
+    );
 
     expect(screen.getByText(/Trivia.*Quiz.*Time/i)).toBeTruthy();
   });
 
   it("should render Button component with Let's Play label", () => {
-    render(<StartScreen />);
+    render(
+      <QuizProvider>
+        <StartScreen />
+      </QuizProvider>,
+    );
 
     expect(screen.findByText(/Let's Play/i)).toBeTruthy();
   });
