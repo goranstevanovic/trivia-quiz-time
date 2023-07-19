d36b3d03ef2a00241a2e86f7f35a53a39887bf70
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-19 18:05:45 +0200

    Style correct and incorrect answers

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -4,15 +4,16 @@ import styles from './Question.module.css';
 import { shuffleArray } from '../utils/arrayUtils';
 
 export default function Question() {
-  const { questions, currentQuestionIndex, dispatch } = useQuiz();
+  const { questions, currentQuestionIndex, selectedAnswer, dispatch } =
+    useQuiz();
   const currentQuestion = questions.at(currentQuestionIndex) as QuestionType;
 
   const answers = [
     currentQuestion?.correct_answer,
     ...currentQuestion.incorrect_answers,
   ];
-
   const shuffledAnswers = shuffleArray(answers);
+  const isAnswered = selectedAnswer !== '';
 
   return (
     <div className={styles.questionWrapper}>
@@ -25,7 +26,20 @@ export default function Question() {
         {shuffledAnswers.map((answer) => (
           <li key={answer}>
             <button
-              className={styles.answerOption}
+              className={`${styles.answerOption} ${
+                isAnswered && answer === selectedAnswer
+                  ? selectedAnswer === currentQuestion.correct_answer
+                    ? styles.answerCorrect
+                    : styles.answerIncorrect
+                  : ''
+              } ${
+                isAnswered && answer !== selectedAnswer
+                  ? answer === currentQuestion.correct_answer
+                    ? styles.answerCorrect
+                    : ''
+                  : ''
+              }`}
+              disabled={isAnswered}
               onClick={() => {
                 if (dispatch) {
                   dispatch({ type: 'newAnswer', payload: answer });
