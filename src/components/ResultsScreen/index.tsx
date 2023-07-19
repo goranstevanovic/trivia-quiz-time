a93f3022b9df0738a56a75d3cafc979074bebe2f
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-19 19:49:45 +0200

    Replace dummy data with state data

--- src/components/ResultsScreen/index.tsx
+++ src/components/ResultsScreen/index.tsx
@@ -1,18 +1,25 @@
+import { useQuiz } from '../../contexts/QuizContext';
 import Button from '../Button';
 import styles from './ResultsScreen.module.css';
 
 export default function ResultsScreen() {
+  const { questions, correctAnswers, points } = useQuiz();
+  const totalAnswers = questions.length;
+  const correctAnswersPercent = (correctAnswers / totalAnswers) * 100;
+
   return (
     <div className={styles.resultsScreen}>
       <div className={styles.resultsWrapper}>
         <h1 className={styles.resultsHeading}>Results</h1>
         <div>
           <h2 className={styles.resultsTitle}>Total correct answers:</h2>
-          <p className={styles.resultsStats}>40/50 &ndash; 80%</p>
+          <p className={styles.resultsStats}>
+            {correctAnswers}/{totalAnswers} &ndash; {correctAnswersPercent}%
+          </p>
         </div>
         <div>
           <h2 className={styles.resultsTitle}>Total points:</h2>
-          <p className={styles.resultsStats}>1,000/1,500 &ndash; 67%</p>
+          <p className={styles.resultsStats}>{points}/1,500 &ndash; 67%</p>
         </div>
       </div>
 
