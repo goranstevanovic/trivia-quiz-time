1854e6f753d58176b3a267db183d808935c8c112
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-22 13:45:30 +0200

    Remove Start Screen button

--- src/components/SettingsScreen/index.tsx
+++ src/components/SettingsScreen/index.tsx
@@ -1,23 +1,10 @@
-import { useQuiz } from '../../contexts/QuizContext';
-import Button from '../Button';
 import SettingsForm from '../SettingsForm';
 import styles from './SettingsScreen.module.css';
 
 export default function SettingsScreen() {
-  const { dispatch } = useQuiz();
-
   return (
     <div className={styles.settingsScreen}>
       <SettingsForm />
-      <Button
-        onClick={() => {
-          if (dispatch) {
-            dispatch({ type: 'startQuiz' });
-          }
-        }}
-      >
-        Start Quiz
-      </Button>
     </div>
   );
 }
