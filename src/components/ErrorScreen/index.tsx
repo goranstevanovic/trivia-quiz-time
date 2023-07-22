87e6a55c14958dd1b0a0f3d3361a787baae6427c
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-22 17:39:35 +0200

    Add Try Again button that shows the Settings screen

--- src/components/ErrorScreen/index.tsx
+++ src/components/ErrorScreen/index.tsx
@@ -1,10 +1,23 @@
+import { useQuiz } from '../../contexts/QuizContext';
+import Button from '../Button';
+
 export default function ErrorScreen() {
+  const { dispatch } = useQuiz();
+
   const message =
     'Something went wrong while trying to get questions and answers for your game of quiz. Please try again.';
+
+  function handleTryAgainClick() {
+    if (dispatch) {
+      dispatch({ type: 'showSettings' });
+    }
+  }
 
   return (
     <div>
       <p>{message}</p>
+
+      <Button onClick={handleTryAgainClick}>Try Again</Button>
     </div>
   );
 }
