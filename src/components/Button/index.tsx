41b2ed75b722209be5a606a97aae3bee7b3b44c2
Goran Stevanovic <stevanovicgoran@proton.me>
2023-07-22 18:50:50 +0200

    Add optional disabled prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,17 +4,23 @@ type ButtonSize = 'big' | 'small';
 
 type ButtonProps = {
   size?: ButtonSize;
+  disabled?: boolean;
   children: React.ReactNode;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 export default function Button({
   size = 'big',
+  disabled = false,
   onClick,
   children,
 }: ButtonProps) {
   return (
-    <button className={`${styles.button} ${styles[size]}`} onClick={onClick}>
+    <button
+      className={`${styles.button} ${styles[size]}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
