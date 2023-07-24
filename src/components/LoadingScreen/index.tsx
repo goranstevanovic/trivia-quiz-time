cac37f3fb00ce2e6984ccc60d3e8adbbf76e101d
Goran Stevanovic <110386061+goranstevanovic@users.noreply.github.com>
2023-07-24 13:53:07 +0200

    Set maximum width (#49)
    Maximum width is set so the text doesn't get too wide on bigger screens

--- src/components/LoadingScreen/index.tsx
+++ src/components/LoadingScreen/index.tsx
@@ -1,5 +1,9 @@
 import styles from './LoadingScreen.module.css';
 
 export default function LoadingScreen() {
-  return <div className={styles.loader}></div>;
+  return (
+    <div className={styles.loadingScreen}>
+      <div className={styles.loader}></div>
+    </div>
+  );
 }
